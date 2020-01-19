9e9a8530d82af2a5e4ebfcde411e5e6bf50badfa
Kenzie <clarkmackenzieb@gmail.com>
2020-01-19 13:01:06 -0800

    add port and server for heroku so maybe this time it won't kill the bot???

--- bot.js
+++ bot.js
@@ -11,6 +11,13 @@ const ainzMemberRoleId = "525080674219196428";
 
 const ainzRoleString = "ainz";
 
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, function() {
+  console.log("Server started.......");
+})
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
